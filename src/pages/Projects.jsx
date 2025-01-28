e1ca9c1b00853539899468097c63d60520a4d426
adi <aditya@mail.com>
2025-01-28 19:38:58 +0530

    styling fixed

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -22,43 +22,9 @@ const ProjectSection = () => {
 
       <div className="project-container">
         {/* Project 1 */}
-        <div className="project-card project1">
-          <div className="project-number project-number-right">
-            01
-          </div>
-          <div className="project-content project-content-left">
-            <div className="project-skill-container">
-                <img className="project-skill" src={html} alt=""/>
-                <img className="project-skill" src={css} alt=""/>
-                <img className="project-skill" src={js} alt=""/>
-                <img className="project-skill" src={exp} alt=""/>
-                {/* <img className="project-skill" src={next} alt=""/> */}
-                <img className="project-skill" src={tail} alt=""/>
-                <img className="project-skill" src={njs} alt=""/>
-                <img className="project-skill" src={mongo} alt=""/>
-                <img className="project-skill" src={red} alt=""/>
-                {/* <img className="project-skill" src={vercel} alt=""/> */}
-          </div>
-                <h2 class="project-heading md:leading-12 leading-1">StudyNotion</h2>
-                  <div class="project-subheading md:block hidden">
-                  StudyNotion is a fully functional ed-tech platform that enables users to create, consume, and rate educational content. The platform is built using the MERN stack, which includes ReactJS, NodeJS, MongoDB, and ExpressJS.
-                  </div>
-                  <div class="btn-grp">
-                    <Link
-                    to={"http://github.com"}
-                    class="btn-pink btn-project">
-                      <i title="GitHub Link" class="fa-brands fa-github icon">
-                        <FaGithub/>
-                      </i>
-                      Read More
-                    </Link>
-                  </div>
-          </div>
-        </div>
-        {/* project 2 */}
         <div className="project-card project2">
           <div className="project-number project-number-left">
-            02
+            01
           </div>
           <div className="project-content project-content-right">
             <div className="project-skill-container">
@@ -84,7 +50,9 @@ const ProjectSection = () => {
                       <i title="GitHub Link" class="fa-brands fa-github icon">
                         <FaGithub/>
                       </i>
+                      <p className="md:block hidden">
                       Read More
+                      </p>
                     </Link>
                   </div>
           </div>
@@ -92,7 +60,7 @@ const ProjectSection = () => {
         {/* project 3 */}
         <div className="project-card project3">
           <div className="project-number project-number-right">
-            03
+            02
           </div>
           <div className="project-content project-content-left">
           <div className="project-skill-container">
@@ -117,8 +85,9 @@ const ProjectSection = () => {
                     class="btn-pink btn-project">
                       <i title="GitHub Link" class="fa-brands fa-github icon">
                         <FaGithub/>
-                      </i>
+                      </i><p className="md:block hidden">
                       Read More
+                      </p>
                     </Link>
                     <a href="">
                       <i title="GitHub Link" class="fa-brands fa-github icon"></i>
@@ -129,7 +98,7 @@ const ProjectSection = () => {
         {/* project 4 */}
         <div className="project-card project4">
           <div className="project-number project-number-left">
-            04
+            03
           </div>
           <div className="project-content project-content-right">
           <div className="project-skill-container">
@@ -155,11 +124,48 @@ const ProjectSection = () => {
                       <i title="GitHub Link" class="fa-brands fa-github icon">
                         <FaGithub/>
                       </i>
+                      <p className="md:block hidden">
                       Read More
+                      </p>
                     </Link>
                     <a href="">
                       <i title="GitHub Link" class="fa-brands fa-github icon"></i>
                     </a>
+                  </div>
+          </div>
+        </div>
+        <div className="project-card project1">
+          <div className="project-number project-number-right">
+            04
+          </div>
+          <div className="project-content project-content-left">
+            <div className="project-skill-container">
+                <img className="project-skill" src={html} alt=""/>
+                <img className="project-skill" src={css} alt=""/>
+                <img className="project-skill" src={js} alt=""/>
+                <img className="project-skill" src={exp} alt=""/>
+                {/* <img className="project-skill" src={next} alt=""/> */}
+                <img className="project-skill" src={tail} alt=""/>
+                <img className="project-skill" src={njs} alt=""/>
+                <img className="project-skill" src={mongo} alt=""/>
+                <img className="project-skill" src={red} alt=""/>
+                {/* <img className="project-skill" src={vercel} alt=""/> */}
+          </div>
+                <h2 className="project-heading">StudyNotion</h2>
+                  <div class="project-subheading md:block hidden">
+                  StudyNotion is a fully functional ed-tech platform that enables users to create, consume, and rate educational content. The platform is built using the MERN stack, which includes ReactJS, NodeJS, MongoDB, and ExpressJS.
+                  </div>
+                  <div className="btn-grp">
+                    <Link
+                    to={"http://github.com"}
+                    className="btn-pink btn-project">
+                      <i title="GitHub Link" class="fa-brands fa-github icon">
+                        <FaGithub/>
+                      </i>
+                      <p className="md:block hidden">
+                      Read More
+                      </p>
+                    </Link>
                   </div>
           </div>
         </div>
