aae7d67024a1febfbd8f44d78de52b4941cb148c
adi <aditya@mail.com>
2025-01-28 17:43:43 +0530

    changed name

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 require("dotenv").config()
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 
 // Middleware
 app.use(cors());
